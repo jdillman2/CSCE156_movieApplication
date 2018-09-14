3b27098f21d84dbe28acb127b3a80b911ed72871
jesse <jesse@10.211.143.88>
2018-09-14 12:52:49 -0500

    First Commit

---
+++ src/unl/cse/movieApp/phase1/Address.java
@@ -0,0 +1,55 @@
+package unl.cse.movieApp.phase1;
+
+public class Address {
+	String street = null;
+	String city = null;
+	String state = null;
+	String zip = null;
+	String country = null;
+	
+	public Address(String street, String city, String state, String zip, String country) {
+		super();
+		this.street = street;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.country = country;
+	}
+	
+	public Address() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getZip() {
+		return zip;
+	}
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	
+}
---
+++ src/unl/cse/movieApp/phase1/Person.java
@@ -0,0 +1,51 @@
+package unl.cse.movieApp.phase1;
+
+public class Person {
+	
+	String personCode = null;
+	String lName = null;
+	String fName = null;
+	Address personAddress = new Address();
+	String emails[] = null;
+	public Person(String personCode, String lName, String fName, Address personAddress, String[] emails) {
+		super();
+		this.personCode = personCode;
+		this.lName = lName;
+		this.fName = fName;
+		this.personAddress = personAddress;
+		this.emails = emails;
+	}
+	public String getPersonCode() {
+		return personCode;
+	}
+	public void setPersonCode(String personCode) {
+		this.personCode = personCode;
+	}
+	public String getlName() {
+		return lName;
+	}
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+	public String getfName() {
+		return fName;
+	}
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+	public Address getPersonAddress() {
+		return personAddress;
+	}
+	public void setPersonAddress(Address personAddress) {
+		this.personAddress = personAddress;
+	}
+	public String[] getEmails() {
+		return emails;
+	}
+	public void setEmails(String[] emails) {
+		this.emails = emails;
+	}
+	
+	
+	
+}
